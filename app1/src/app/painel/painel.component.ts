e5712810a07d941e9108194312c8695ed9a6ccae
JoelSpinelli <joel.spinelli@hotmail.com>
2018-05-04 15:53:46 -0300

    Event-Binding junto property binding entre componentes

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -12,10 +12,41 @@ export class PainelComponent implements OnInit {
 
   public frases: Array<Frase> = frases;
   public instrucao: String = 'Traduza a frase:';
+  public resposta: String;
 
-  constructor() { console.log(this.frases); }
+  public rodada: number;
+  public rodadaFrase: Frase;
+
+  public progresso: number;
+
+  constructor() {
+    this.rodada = 0;
+    this.progresso = 0;
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.frases);
+  }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  public verificarResposta(): void {
+
+    // trocar a pergunta da rodada
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
+      this.rodada++;
+
+      this.progresso += (100 / this.frases.length);
+      console.log(this.progresso);
+
+      this.rodadaFrase = this.frases[this.rodada];
+      alert('A tradução está correta');
+    } else {
+      alert('A tradução está errada');
+    }
+
+  }
 }
